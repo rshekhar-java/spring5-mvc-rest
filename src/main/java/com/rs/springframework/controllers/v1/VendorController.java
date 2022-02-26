18924ab217ecc9a26119313a927b3a90e461dfda
Ravi Shekhar <rsshekhar27@outlook.com>
2022-02-26 14:33:51 -0600

    Added Custom Info on VendorDTO and VendorController

--- src/main/java/com/rs/springframework/controllers/v1/VendorController.java
+++ src/main/java/com/rs/springframework/controllers/v1/VendorController.java
@@ -3,6 +3,8 @@ package com.rs.springframework.controllers.v1;
 import com.rs.springframework.api.v1.model.VendorDTO;
 import com.rs.springframework.api.v1.model.VendorListDTO;
 import com.rs.springframework.services.VendorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
  * created by rs 2/25/2022.
  */
 @RestController
+@Api(description = "This is my Vendor API")
 @RequestMapping(VendorController.BASE_URL)
 public class VendorController {
 
@@ -21,6 +24,7 @@ public class VendorController {
         this.vendorService = vendorService;
     }
 
+    @ApiOperation(value = "View List of Vendors", notes="These are some API Notes")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public VendorListDTO getVendorList(){
