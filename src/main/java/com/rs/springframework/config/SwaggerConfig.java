5f0696925664f2e9d27e8fcf848daff1bdd74766
Ravi Shekhar <rsshekhar27@outlook.com>
2022-02-26 13:20:02 -0600

    Updated swagger ui config and added API info in metadata

--- src/main/java/com/rs/springframework/config/SwaggerConfig.java
+++ src/main/java/com/rs/springframework/config/SwaggerConfig.java
@@ -2,20 +2,24 @@ package com.rs.springframework.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.*;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.ArrayList;
 
 /**
  * created by rs 2/25/2022.
  */
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket api(){
@@ -24,16 +28,58 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
     }
 
-    @Override
+    private ApiInfo metaData(){
+
+        Contact contact = new Contact("Ravi Shekhar", "https://github.com/rshekhar-java",
+                "rsshekhar27@outlook.com");
+
+        return new ApiInfo(
+                "Spring Framework Developer",
+                "Spring Framework 5: Beginner to Guru",
+                "1.0",
+                "Terms of Service: Some Data about Spring5 MVC REST",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+    }
+    //for < 3.0.0 version
+/*    @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }*/
+
+/*    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/swagger-ui**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }*/
+@Override
+public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    registry
+            .addResourceHandler("swagger-ui")
+            .addResourceLocations("classpath:/META-INF/resources/");
+
+    registry
+            .addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+}
 
 }
