4249f87e4af55be29fa647c5c1f02835b30c3c9c
Ravi Shekhar <rsshekhar27@outlook.com>
2022-02-28 20:29:22 -0600

    added pom dependencies for xml parsing

--- src/main/java/com/rs/springframework/Spring5MvcRestApplication.java
+++ src/main/java/com/rs/springframework/Spring5MvcRestApplication.java
@@ -3,11 +3,8 @@ package com.rs.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-
-//@ComponentScan({"com.rs.springframework.api.v1"})
-//@EnableJpaRepositories("com.rs.springframework.repositories")
 @SpringBootApplication
 public class Spring5MvcRestApplication {
 
